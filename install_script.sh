0b9bd9997653d0bd1f1f7316be689d0e0fa41a8a
torosyatko <100668227+torosyatko@users.noreply.github.com>
2022-03-03 15:31:10 +0200

    add install script

---
+++ install_script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update && sudo apt install -y git mcedit apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt update && apt-cache policy docker-ce && sudo apt install -y docker-ce && sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+rm -rf dosya
+git clone https://github.com/torosyatko/dosya.git
+cd dosya
+mv .env-example .env
+sudo docker-compose up -d torserv
+sudo docker-compose up torosya
